87449e7cef7c3505173362ac3c8a22e31a9163c4
Marcos Alexandre Reis Torquato dos Santos <marcos.alex1002@gmail.com>
2023-08-17 19:58:12 -0300

    Criação arquivos fonte

---
+++ Devbot/src/me/marcostec/devbot/DevBot.java
@@ -0,0 +1,13 @@
+package me.marcostec.devbot;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import javax.security.auth.login.LoginException;
+import java.util.EnumSet;
+
+public class DevBot {
+    
+
+}
